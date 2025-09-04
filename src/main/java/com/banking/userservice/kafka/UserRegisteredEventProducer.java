6c44581be096aa916b25c2ebaaa2a823b854eae3
Roopa4112 <126701241+Roopa4112@users.noreply.github.com>
2025-09-04 12:40:25 +0530

    Update UserRegisteredEventProducer.java

--- src/main/java/com/banking/userservice/kafka/UserRegisteredEventProducer.java
+++ src/main/java/com/banking/userservice/kafka/UserRegisteredEventProducer.java
@@ -1,18 +1,28 @@
 package com.banking.userservice.kafka;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserRegisteredEventProducer {
+    ObjectMapper objectMapper = new ObjectMapper();
 
-    @Autowired
-    private KafkaTemplate<String, UserRegisteredEvent> kafkaTemplate;
-
+    private final KafkaTemplate<String, String> kafkaTemplate;
     private static final String TOPIC = "user-registered-topic";
 
+    public UserRegisteredEventProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
     public void sendUserRegisteredEvent(UserRegisteredEvent event) {
-        kafkaTemplate.send(TOPIC, event);
+        String json= null;
+        try {
+            json = objectMapper.writeValueAsString(event);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        kafkaTemplate.send(TOPIC, json);
     }
 }
